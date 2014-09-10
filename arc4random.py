f3c33fbfad526fa9a49e7971b6d70ea84b111f66
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-09-10 02:22:39 -0700

    added comment about RFC4345

--- arc4random.py
+++ arc4random.py
@@ -2,23 +2,28 @@
 
 import random
 
+@profile
 def arc4random():
 	key = random.sample(range(256), 256) # something
 	seeds = _RC4PRGA(_RC4keySchedule(key))
 	return (seeds[0]<<24)|(seeds[1]<<16)|(seeds[2]<<8)|seeds[3]
 
+@profile
 def _RC4keySchedule(key):
 	sbox = list(range(256))
 	x = 0
+	keySize = len(key)
 	for i in sbox:
-		x = (x+i+key[i%len(key)])%256
+		x = (x+i+key[i%keySize])%256
 		_swap(sbox, i, x)
 	return sbox
 
+@profile
 def _RC4PRGA(state):
 	x, y = 0, 0
 	seeds = []
-	for i in range((1536)+4):
+	# Discard first 1536 bytes of the keystream according to RFC4345 as they may reveal information about key used (a set of these keys could reveal information about the source for our key)
+	for i in range((1536//4)+4):
 		x = (x+1)%256
 		y = (y+state[x])%256
 		_swap(state, x, y)
@@ -26,5 +31,8 @@ def _RC4PRGA(state):
 			seeds.append(state[(state[x]+state[y])%256])
 	return seeds
 
+@profile
 def _swap(listy, n1, n2):
 	listy[n1], listy[n2] = listy[n2], listy[n1]
+
+arc4random()
